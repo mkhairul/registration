261b09095a56a8b1f3c76808fd235c17e27b41f0
mkhairul <mkhairul@gmail.com>
2015-05-04 02:35:26 +0800

    initial commit

---
+++ resources/views/homepage.php
@@ -0,0 +1,30 @@
+<div class="w-row">
+    <div class="w-col w-col-9">
+        <div class="contact-map-fprm">
+            <h4 class="white-text-footer contact">CONTACT INFO</h4>
+            <div class="w-form form-wrapper">
+                <form class="w-clearfix" id="email-form" name="email-form" data-name="Email Form" action="contact.php" method="post">
+                    <input class="w-input text-field" id="name-2" type="text" placeholder="Enter your name" name="name-2" data-name="Name 2" required="required">
+                    <input class="w-input text-field" id="email-3" type="email" placeholder="Enter your email address" name="email-3" data-name="Email 3" required="required">
+                    <input class="w-input text-field" id="email-4" type="text" placeholder="Subject Mail" name="email-4" data-name="Email 4" required="required">
+                    <textarea class="w-input text-area" id="field-2" placeholder="Write message" name="field-2" required="required" data-name="Field 2"></textarea>
+                    <input class="w-button submit-button" type="submit" value="Submit Form" data-wait="Please wait...">
+                </form>
+                <div class="w-form-done success-message">
+                    <p>Thank you! Your submission has been received!</p>
+                </div>
+                <div class="w-form-fail error">
+                    <p>Oops! Something went wrong while submitting the form :(</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="w-col w-col-3">
+        <h4 class="white-text-footer contact">CONTACT INFO</h4>
+        <p class="text-fotter">PO Box 17122 Nollins Street West Arizona Victoria 5034 Los Angelos
+            <br>
+            <br>Phone: 1.800.555.6789
+            <br>Email:&nbsp;support@minimo.com
+            <br>Web:&nbsp;minimo.com</p>
+    </div>
+</div>
